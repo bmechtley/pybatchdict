7ccaad75c0b918e6f53ea7d0d4fe8a3dc5d676d8
plant <bmechtley@gmail.com>
2013-09-24 11:15:56 +0100

    actual module, support for python 3

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pybatchdict',
+    version='0.2',
+    description='combinatorial dictionary generator for batch processes',
+    author='Brandon Mechtley',
+    author_email='bmechtley@gmail.com',
+    url='http://github.com/plant/pybatchdict/',
+    packages=['pybatchdict']
+)
+
